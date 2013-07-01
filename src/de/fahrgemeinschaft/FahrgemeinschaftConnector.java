fc9f902469ce4e4ba2f0165b10084bafb50434cf
orangeman <flo@teleportr.org>
2013-07-01 20:09:38 +0200

    implement publish

--- src/de/fahrgemeinschaft/FahrgemeinschaftConnector.java
+++ src/de/fahrgemeinschaft/FahrgemeinschaftConnector.java
@@ -64,7 +64,7 @@ public class FahrgemeinschaftConnector extends Connector {
     }
 
     @Override
-    public long getRides(Place from, Place to, Date dep, Date arr) {
+    public long search(Place from, Place to, Date dep, Date arr) {
         
         startDate = df.format(dep);
 
@@ -182,6 +182,12 @@ public class FahrgemeinschaftConnector extends Connector {
             e.printStackTrace();
             return new Date(0);
         }
+    }
+
+    @Override
+    public int publish(Ride offer) {
+        // TODO Auto-generated method stub
+        return 0;
     }
 }
 
